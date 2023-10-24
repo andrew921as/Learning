1b6a0bc51812d890a6e6755b826bcad9799cdc33
Andres Velasco <andresfelipecoronado@gmail.com>
2023-10-24 09:14:58 -0500

    validations

--- Node/class3MiduDev/app.js
+++ Node/class3MiduDev/app.js
@@ -1,10 +1,14 @@
-const express = require('express'); // importamos express -> CommonJS
-const crypto = require('node:crypto'); // importamos crypto -> CommonJS
-const movies = require('./movies.json') // importamos el archivo movies.json
+const express = require('express'); // importamos express -> CommonJS (un framework para crear servidores)
+const crypto = require('node:crypto'); // importamos crypto -> CommonJS (una libreria para generar ids random)
+const movies = require('./movies.json'); // importamos el archivo movies.json
+const { validateMovie } = require('./schemas/movies');
+
 
 const app = express();
 app.disable('x-powered-by'); // Deshabilita el header X-Powered-By: Express
 app.use(express.json()); // Middleware para parsear el body de la request a JSON
+
+
 app.get('/movies', (req, res) => { // Ruta /movies
 	const {genre} = req.query // obtenemos el query param genre
 	if (genre) { // si existe el query param genre	
@@ -25,24 +29,16 @@ app.get('/movies/:id', (req, res) => { // Ruta /movies/:id
 })
 
 app.post('/movies', (req,res)=>{ // Ruta /movies
-	const {
-		title, 
-		genre, 
-		year,
-		director,
-		duration,
-		rate,
-		poster} = req.body // obtenemos el body de la request
+	
+	const result = validateMovie(req.body) // validamos el body de la request
+	
+	if (result.error) {
+		res.status(400).json({error: JSON.parse(result.error.message)})
+	}
 
 		const newMovie ={
 			id: crypto.randomUUID(), // generamos un id random
-			title,
-			genre,
-			director,
-			year,
-			duration,
-			rate: rate ?? 0,
-			poster
+			...result.data
 		}
 
 	movies.push(newMovie)// agregamos la nueva movie
---
+++ Node/class3MiduDev/schemas/movies.js
@@ -0,0 +1,28 @@
+const z = require('zod')// importamos zod -> CommonJS (una libreria para validar el body de la request)
+
+const movieSchema = z.object({
+		title:z.string({
+			invalid_type_error:'Title must be a string',
+			required_error:'Title is required'
+		}),
+		year:z.number().int().min(1888).max(2077),
+		director:z.string(),
+		duration:z.number().int().positive(),
+		rate:z.number().min(0).max(10).default(0),
+		poster:z.string().url({
+			message:'Poster must be a valid url'
+		}),
+		genre:z.array(
+			z.enum(['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy','Film Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Short Film', 'Sport', 'Superhero', 'Thriller', 'War', 'Western']),
+			{
+				required_error:'Genre is required',
+				invalid_type_error:'Movie genre must be an array of enum genres',
+			}
+		).min(1).max(5)
+})
+
+function validateMovie(object){
+	return movieSchema.sarveParse(object)
+}
+
+module.exports = {validateMovie}
